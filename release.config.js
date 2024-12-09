02546cc5f3120f68c4c5aa4db01665ece4bdc114
HallexCosta <hallex.costa@hotmail.com>
2024-12-09 17:52:13 -0300

    chore: adjust message from @semantic-release/git

--- release.config.js
+++ release.config.js
@@ -13,8 +13,19 @@ const config = {
           },
           { path: 'build/main.js.map', label: 'openpix-sdk.js.map' }
         ],
-        message: [
+        message:
           'chore(release): ${nextRelease.version} [skip-ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'build/main.js',
+            label: 'openpix-sdk.js'
+          },
+          { path: 'build/main.js.map', label: 'openpix-sdk.js.map' }
         ]
       }
     ]
