09585f7dd0a197dc6901fa541d7650095b338f94
HallexCosta <hallex.costa@hotmail.com>
2024-12-11 13:31:27 -0300

    feat: use env variables
    BREAKIN CHANGE: in development environment is need use environment
    variable, the expecification was added in .env.example

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,4 @@
-const graphqlURL = 'https://plugin.openpix.com.br/plugin/graphql'
-
 export const config = {
-  appID:
-    'Q2xpZW50X0lkXzBlMWViOWEzLWI4MzktNGRkMy1iOWNmLTI3MGFhOWZlNzEwNzpDbGllbnRfU2VjcmV0X2kzNTdMdlZ2MVlXaEViNm5FYzIzcVNpZ09hZXpKeFJkL2EzR0ZZMVJodkE9',
-  graphqlURL,
-  restApiBaseURL: 'https://api.woovi.com/api/v1'
+  graphqlURL: process.env.GRAPHQL_URL!,
+  restApiBaseURL: process.env.REST_API_BASE_URL!
 }
