bb81716f4853d4aa0282468415502eac89365e0a
HallexCosta <hallex.costa@hotmail.com>
2024-12-16 14:21:12 -0300

    chore(modules/products): remove comments

--- src/app/modules/products/hooks/useProducts.ts
+++ src/app/modules/products/hooks/useProducts.ts
@@ -1,30 +1,21 @@
 import { useState } from 'react'
-// import { reactMemoryAdapter } from '../../openpix-plugin/adapters/reactMemoryAdapter'
 import type { Order } from '../../checkout/context/useCheckout'
 
 export const globalProducts = new Map()
 
 export const useProducts = () => {
-  // console.log('useProducts', { reactMemoryAdapter })
   const [products, setProducts] = useState<Map<string, Order>>(globalProducts)
 
-  // if (!reactMemoryAdapter.states) {
-  //   reactMemoryAdapter.setStates({ products })
-  // }
-
-  const updateProduct = (
-    productId: string | number,
-    data: Record<string, any>
-  ) => {
+  const updateProduct = (productId: string | number, data: Order) => {
     return products.set(String(productId), data)
   }
-  const getProduct = (productId: string | number) => {
-    return products.get(String(productId))
-  }
-  // window.$openpixSDK.add()
+  const getProduct = (productId: string | number): Order | null => {
+    if (!products.has(String(productId))) {
+      return null
+    }
 
-  // window.reactLayer.addProduct = addProduct
-  // window.reactLayer.getProduct = getProduct
+    return products.get(String(productId)) as Order
+  }
 
   return [getProduct, updateProduct, products] as const
 }
